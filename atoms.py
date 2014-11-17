db60cc56d4fd9ace46cbd24a79718c18b9b68e63
Bo Dong <eseshen@gmail.com>
2014-11-16 20:01:11 -0500

    Filter aligned atoms

--- atoms.py
+++ atoms.py
@@ -3,16 +3,22 @@ import sys
 
 
 def parsePdb(path):
-    xyz = []
-    try:
-        # with open(path) as f:
-            # for line in f:
-                # l = line.split()
-                # if l[0] == 'ATOM':
-                    # xyz.append([np.float32(i) for i in l[6:9]])
+	xyz = []
+	try:
+		# with open(path) as f:
+			# for line in f:
+				# l = line.split()
+				# if l[0] == 'ATOM':
+					# xyz.append([np.float32(i) for i in l[6:9]])
 
 		#open the file
 		pdbFile = open(path, 'rt')
+		
+		#get the name of protein
+		line = pdbFile.readline()
+		l = line.split()
+		name = l[-1]
+		# print (name)
 		line = pdbFile.readline()
 		
 		while line:
@@ -25,29 +31,34 @@ def parsePdb(path):
 			xyz.append([np.float32(i) for i in l[6:9]])
 			line = pdbFile.readline()
 			
-    except IOError:
-        print("Could not open file '%s'" % self.path)
-    return xyz
+	except IOError:
+		print("Could not open file '%s'" % self.path)
+	return xyz, name
 
 class Atoms:
-    def __init__(self, atoms):
-        self.xyz = np.matrix(atoms)
-        self.count = len(self.xyz)
-        #if len(self.xyz) > 1:
-        #    print("x\t\ty\t\tz")
-        #    print(self.xyz)
-        #    print("Total atoms = %d" % self.count)
+	def __init__(self, xyz, name = "default"):
+		self.name = name
+		self.xyz = np.matrix(xyz)
+		self.count = len(self.xyz)
 
-    def centroid(self):
-        return self.xyz.mean(0)
+		# if len(self.xyz) > 1:
+		# 	print("x\t\ty\t\tz")
+		# 	print(self.xyz)
+		# 	print("Total atoms = %d" % self.count)
+		# 	print (name)
+		
+	def centroid(self):
+		return self.xyz.mean(0)
 
-    def centerOrigin(self):
-        self.xyz = np.subtract(self.xyz, self.centroid())
+	def centerOrigin(self):
+		self.xyz = np.subtract(self.xyz, self.centroid())
 
-    def translate(self, t):
-        self.xyz = np.add(self.xyz, t)
-
+	def translate(self, t):
+		self.xyz = np.add(self.xyz, t)
+		
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        path = sys.argv[1]
-        atoms = Atoms(parsePdb(path))
+	if len(sys.argv) > 1:
+		path = sys.argv[1]
+		xyz, name = parsePdb(path)
+		atoms = Atoms(xyz, name)
+		print atoms.name
--- wRMSD.py
+++ wRMSD.py
@@ -5,13 +5,69 @@ from atoms import parsePdb
 from horns import horn
 import math
 
+def filter (atom_1, atom_2, ali):
+	seq_1 = ""
+	seq_2 = ""
+
+	xyz_1 = []
+	xyz_2 = []
+
+	# extract the proteins
+	for line in ali:
+		if (line.startswith(atom_1.name) and line[5] == 'A'):
+			l = line.split()
+			seq_1 += l[1]
+		if (line.startswith(atom_2.name) and line[5] == 'A'):
+			l = line.split()
+			seq_2 += l[1]			
+
+	count_1 = 0
+	count_2 = 0
+	mask_1 = []
+	mask_2 = []
+	
+	# find aligned atoms
+	for i in range(len(seq_1)):
+		if (seq_1[i] != '-'):
+			if (seq_2[i] != '-'):
+				mask_1.append(count_1)
+				mask_2.append(count_2)
+				count_2 += 1
+			count_1 += 1
+		else:
+			if (seq_2[i] != '-'):
+				count_2 += 1
+	# print (mask_1)
+	# print (mask_2)
+	
+	# generate new xyz
+	xyz_1 = atom_1.xyz[mask_1]
+	xyz_2 = atom_2.xyz[mask_2]
+	
+	return Atoms(xyz_1), Atoms(xyz_2)
+
 if __name__ == "__main__":
-	if len(sys.argv) < 2:
+	if len(sys.argv) < 3:
 		print("Enter 2 .pdb files to compare")
 		sys.exit()
 
+
+	# load alignment
+	try: 
+		ali = open(sys.argv[3], 'rt')
+	except IOError:
+		print("Could not open file '%s'" % sys.argv[3])
+		
 	# get the 2 proteins loaded
-	pro = [Atoms(parsePdb(sys.argv[1])), Atoms(parsePdb(sys.argv[2]))]
+	xyz, name = parsePdb(sys.argv[1])
+	atom_1 = Atoms(xyz, name)
+	xyz, name = parsePdb(sys.argv[2])	
+	atom_2 = Atoms(xyz, name)
+	
+	# filter non-aligned atoms
+	atom_1, atom_2 = filter (atom_1, atom_2, ali)
+	
+	pro = [atom_1, atom_2]
 	mmin = min(pro[0].count, pro[1].count)
 	### STEP 1 ###
 	for p in pro: # resize
@@ -21,7 +77,7 @@ if __name__ == "__main__":
 
 	### STEP 2 ###
 	# average Structure
-	avgS = Atoms(pro[0].xyz*0) # initialize all 0s
+	avgS = Atoms(np.zeros(pro[0].xyz.shape)) # initialize all 0s
 	for p in pro:
 		avgS.xyz += p.xyz
 	avgS.xyz = avgS.xyz/len(pro) # average
