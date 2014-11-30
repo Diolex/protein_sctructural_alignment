4e05939dba0e6e8a3bca7a4e41c908656d83317f
Bo Dong <eseshen@gmail.com>
2014-11-29 23:19:55 -0500

    Bug fixes
    Bug fix in readPIM

--- readPim.py
+++ readPim.py
@@ -2,13 +2,14 @@
 
 c = [0]*10
 with open('results.pim', 'r') as pim:
-    for line in pim:
-        l = line.split()
-        if len(l) > 15:
-            for n in range(2, len(l)):
-                for m in range(0,10):
-                    if(float(n) > m*10 and float(n) <= (m+1)*10):
-                        c[m] += 1
+	for line in pim:
+		l = line.split()
+		if len(l) > 15:
+			for n in range(3, len(l)):
+				num = float(l[n])
+				for m in range(0,10):
+					if(num > m*10 and float(n) <= (m+1)*10):
+						c[m] += 1
 
 for m in range(0,len(c)):
-    print("%.2f:%d" % (m*10.0, c[m]))
+	print("%.2f:%d" % (m*10.0, c[m]))
