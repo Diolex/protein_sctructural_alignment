e2bcda6cccf42c7c7968bf66b93eb5a9f3dd6b0c
Bo Dong <eseshen@gmail.com>
2014-12-04 18:04:11 -0500

    reshape more CSV results

--- reshapePIM.py
+++ reshapePIM.py
@@ -1,24 +1,26 @@
 import csv
+import sys
 
-c = []
-with open('results.pim', 'r') as pim:
-	names = []
-	for line in pim:
-		l = line.split()
-		if len(l) > 15:
-			names.append(l[1])
-			c.append(l[2:len(l)])
-			
-with open('wRMSD.csv', 'rb') as input:
-	with open('compare.csv', 'wb') as csvfile:
-		fieldnames = ['Protein A', 'Protein B','wRMSD', 'PI']
-		reader = csv.reader(input)
-		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)	
-		for row in reader:
-			name_A = row[0]
-			name_B = row[1]
-			wRMSD = row[2]
-			i = names.index(name_A)
-			j = names.index(name_B)
-			writer.writerow({'Protein A': name_A, 'Protein B': name_B, 'wRMSD':wRMSD,  'PI': c[i][j]})
+if __name__ == "__main__":
+	c = []
+	with open('results.pim', 'r') as pim:
+		names = []
+		for line in pim:
+			l = line.split()
+			if len(l) > 15:
+				names.append(l[1])
+				c.append(l[2:len(l)])
+				
+	with open(sys.argv[1], 'rb') as input:
+		with open('results/' + sys.argv[1], 'wb') as csvfile:
+			fieldnames = ['Protein A', 'Protein B','wRMSD', 'PI']
+			reader = csv.reader(input)
+			writer = csv.DictWriter(csvfile, fieldnames=fieldnames)	
+			for row in reader:
+				name_A = row[0]
+				name_B = row[1]
+				wRMSD = row[2]
+				i = names.index(name_A)
+				j = names.index(name_B)
+				writer.writerow({'Protein A': name_A, 'Protein B': name_B, 'wRMSD':wRMSD,  'PI': c[i][j]})
 
